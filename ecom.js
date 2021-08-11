b3afa7e13db2b19d0429eee9844f9a17c80ad44b
rscool <rbaghel@ch.iitr.ac.in>
2021-08-11 19:35:54 +0530

    v1.8

--- ecom.js
+++ ecom.js
@@ -1,1 +1,39 @@
+var x = document.getElementById("btn0");
+function change(){
+    x.style.background = "blue";
+}
+x.addEventListener("click", change);
 
+// let carts = document.querySelectorAll('each-card');
+// for(let i=0; i<carts.length; i++){
+//    console.log("hiii");
+// }
+
+// var x = document.getElementById("btn0");
+// x.style.color = "red";
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
